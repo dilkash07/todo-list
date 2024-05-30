5add07a537619c20a187af064904a1dbd90fc616
Dilkash Raza <dilkashsns@gmail.com>
2024-05-30 08:32:16 +0530

    Todo List

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function () {
+  addTask();
+});
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    const li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("todoData", listContainer.innerHTML);
+}
+
+function showData() {
+  listContainer.innerHTML = localStorage.getItem("todoData");
+}
+showData();
